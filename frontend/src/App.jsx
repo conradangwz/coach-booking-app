be2981bbf0691df3bad1fbb36c6ff16d8decde79
cang688 <cang688@aucklanduni.ac.nz>
2026-03-14 23:02:40 +1300

    began creating the appointment page for each coach

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/contact' element={<Contact />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/appointments/:coachId' element={<Appointment />} />
+        <Route path='/appointment/:coachId' element={<Appointment />} />
       </Routes>
       <Footer />
     </div>
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appointment = () => {
+
+    const { coachId } = useParams()
+    const { coaches } = useContext(AppContext)
+
+    const [coachInfo, setCoachInfo] = useState(null)
+
+    const fetchCoachInfo = async () => {
+        const coachInfo = coaches.find(coach => coach._id === coachId)
+        setCoachInfo(coachInfo)
+        console.log(coachInfo)
+
+    }
+
+    useEffect(() => {
+        fetchCoachInfo()
+    }, [coaches, coachId])
+
     return (
         <div>
 
