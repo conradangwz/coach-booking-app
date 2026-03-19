3aedbf8d3fd5f293fc3436c2b6c9763b7715c179
Conrad <cang688@aucklanduni.ac.nz>
2026-03-19 23:50:34 +1300

    completed the ui for the my bookings part

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,21 +9,21 @@ const MyAppointments = () => {
         <div>
             <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Bookings</p>
             <div>
-                {coaches.slice(0, 2).map((item, index) => (
+                {coaches.slice(0, 3).map((item, index) => (
                     <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
                         <img className='w-32 bg-indigo-50' src={item.image} alt="" />
-                        <div className='flex-1 text-sm text-black-800'>
-                            <p>{item.name}</p>
+                        <div className='flex-1 text-sm text-black-800 mt-3'>
+                            <p className='text-neutral-700 font-semibold'>{item.name}</p>
                             <p>{item.speciality}</p>
-                            <p>Address:</p>
-                            <p>{item.address.line1}</p>
-                            <p>{item.address.line2}</p>
-                            <p><span>Date & Time:</span>06, July, 2026 | 11:30 PM</p>
+                            <p className='text-black-700 font-medium mt-1'>Address:</p>
+                            <p className='text-xs'>{item.address.line1}</p>
+                            <p className='text-xs'>{item.address.line2}</p>
+                            <p className='text-sm mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time: </span>06, July, 2026 | 11:30 PM</p>
                         </div>
                         <div></div>
-                        <div>
-                            <button>Pay Online</button>
-                            <button>Cancel Booking</button>
+                        <div className='flex flex-col gap-2 justify-end'>
+                            <button className='text-sm text-neutral-700 text-center sm:min-w-48 py-2 border border-neutral-500 rounded hover:bg-secondary hover:text-white transition-all duration-300'>Pay Online</button>
+                            <button className='text-sm text-neutral-700 text-center sm:min-w-48 py-2 border border-neutral-500 rounded hover:bg-primary hover:text-white transition-all duration-300'>Cancel Booking</button>
                         </div>
                     </div>
                 ))}
