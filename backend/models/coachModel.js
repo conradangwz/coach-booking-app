d4fd83adfee7229839724e594efefca56867faff
cang688 <cang688@aucklanduni.ac.nz>
2026-03-29 21:29:46 +1300

    configured connection to db and cloudinary, made schema model for user and coach

---
+++ backend/models/coachModel.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const coachSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    speciality: {
+        type: String,
+        required: true
+    },
+    degree: {
+        type: String,
+        required: true
+    },
+    experience: {
+        type: String,
+        required: true
+    },
+    about: {
+        type: String,
+        required: true
+    },
+    availabile: {
+        type: Boolean,
+        required: true
+    },
+    fee: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: Object,
+        required: true
+    },
+    date: {
+        type: Number,
+        required: true
+    },
+    slots_booked: {
+        type: Object,
+        default: {}
+    }
+}, { minimize: false })
+
+const coachModel = mongoose.models.coach || mongoose.model("coach", coachSchema)
+
+export default coachModel;
